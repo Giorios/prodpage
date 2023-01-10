4f8c300982628069a24f989b5b4b158b89bf4065
Giorios <90624324+Giorios@users.noreply.github.com>
2023-01-10 15:59:41 +0000

    everything added

---
+++ script.js
@@ -0,0 +1,124 @@
+function wSco() {
+  document.getElementById('product-photo').src = "prodpic/sc1.jpg"
+
+      document.getElementById('price').style.color = 'black'
+};
+function dgSco() {
+  document.getElementById('product-photo').src = "prodpic/sc1dg.jpg"
+
+      document.getElementById('price').style.color = 'black'
+};
+function maSco() {
+  document.getElementById('product-photo').src = "prodpic/sc1ma.jpg"
+
+    document.getElementById('price').style.color = 'black'
+};
+function drSco() {
+  document.getElementById('product-photo').src = "prodpic/sc1dr.jpg"
+
+      document.getElementById('price').style.color = 'black'
+};
+
+
+function bSct() {
+  document.getElementById('product-photo').src = "prodpic/sc2b.jpg"
+
+      document.getElementById('price').style.color = 'black'
+};
+function nSct() {
+  document.getElementById('product-photo').src = "prodpic/sc2n.jpg"
+
+      document.getElementById('price').style.color = 'black'
+};
+function oSct() {
+  document.getElementById('product-photo').src = "prodpic/sc2o.jpg"
+
+    document.getElementById('price').style.color = 'black'
+};
+
+
+function bScth() {
+  document.getElementById('product-photo').src = "prodpic/sc3b.jpg"
+
+      document.getElementById('price').style.color = 'black'
+};
+function fpScth() {
+  document.getElementById('product-photo').src = "prodpic/sc3fp.jpg"
+
+      document.getElementById('price').style.color = 'black'
+};
+function gScth() {
+  document.getElementById('product-photo').src = "prodpic/sc3g.jpg"
+
+    document.getElementById('price').style.color = 'black'
+};
+function mgScth() {
+  document.getElementById('product-photo').src = "prodpic/sc3mg.jpg"
+
+      document.getElementById('price').style.color = 'black'
+};
+function pScth() {
+  document.getElementById('product-photo').src = "prodpic/sc3p.jpg"
+
+  
+      document.getElementById('price').style.color = 'black'
+};
+
+
+function beScf() {
+  document.getElementById('product-photo').src = "prodpic/sc4be.jpg"
+
+      document.getElementById('price').style.color = 'black'
+};
+function blScf() {
+  document.getElementById('product-photo').src = "prodpic/sc4bl.jpg"
+
+      document.getElementById('price').style.color = 'black'
+};
+function gScf() {
+  document.getElementById('product-photo').src = "prodpic/sc4g.jpg"
+
+    document.getElementById('price').style.color = 'black'
+};
+function maScf() {
+  document.getElementById('product-photo').src = "prodpic/sc4ma.jpg"
+
+      document.getElementById('price').style.color = 'black'
+};
+
+
+function blScv() {
+  document.getElementById('product-photo').src = "prodpic/sc5bl.jpg"
+
+      document.getElementById('price').style.color = 'black'
+};
+function bluScv() {
+  document.getElementById('product-photo').src = "prodpic/sc5blu.jpg"
+
+      document.getElementById('price').style.color = 'black'
+};
+function gScv() {
+  document.getElementById('product-photo').src = "prodpic/sc5g.jpg"
+
+    document.getElementById('price').style.color = 'black'
+};
+function rScv() {
+  document.getElementById('product-photo').src = "prodpic/sc5r.jpg"
+
+      document.getElementById('price').style.color = 'black'
+};
+function sScv() {
+  document.getElementById('product-photo').src = "prodpic/sc5s.jpg" 
+};
+
+
+function blScsi() {
+  document.getElementById('product-photo').src = "prodpic/sc6bl.jpg"
+
+      document.getElementById('price').style.color = 'black'
+};
+function burScsi() {
+  document.getElementById('product-photo').src = "prodpic/sc6bur.jpg"
+
+      document.getElementById('price').style.color = 'black'
+};
